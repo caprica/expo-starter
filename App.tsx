10825dffc3ba234fe89848bf801b9ba6bcb6e90d
Mark Lee <mark.lee@capricasoftware.co.uk>
2023-05-26 14:08:44 +0100

    Add safe area provider component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
-            <StatusBar style='auto' />
-        </View>
+        <SafeAreaProvider>
+            <View style={styles.container}>
+                <Text>Open up App.tsx to start working on your app!</Text>
+                <StatusBar style='auto' />
+            </View>
+        </SafeAreaProvider>
     )
 }
 
